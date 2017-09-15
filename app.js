d381873d125b5cef753020bd802b5bfcaa132405
Edgardo Cuello <ecuellog@gmail.com>
2017-09-14 23:15:50 -0700

    Simple Express server created

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+
+port = process.argv[2] || 3000;
+
+var app = express();
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'pug');
+
+app.use(function(req, res, next){
+	res.render('index');
+	return next();
+});
+
+app.listen(port);
+console.log('Express server running on port ');
+console.log(port);
