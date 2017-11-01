e6f73f05429e8cb93090f6b5fcedad9ed2b2e477
Edgardo Cuello <ecuellog@gmail.com>
2017-10-31 22:27:13 -0700

    Set up navbar directive

---
+++ public/directives/navBar.js
@@ -0,0 +1,7 @@
+myApp.directive("navBar", function(){
+	return {
+		replace: true,
+		templateUrl: "public/directives/navBar.html"
+	};
+});
+alert('here');
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var path = require("path");
+//var bodyParser = require("body-parser");
+
+//server routes
+var index = require("./routes/index.js");
+
+var app = express();
+
+app.set("views", path.join(__dirname, "public/views"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+//setup for directories
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))); // redirect bootstrap JS
+app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))); // redirect bootstrap CSS
+
+//routes
+app.use("/", index);
+
+app.use(function(req, res, next){
+  res.end();
+});
+
+//listener
+port = process.argv[2] || 3000;
+app.listen(port, function(){
+  console.log("Listening on port " + port);
+});
+
