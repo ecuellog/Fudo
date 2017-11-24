93f5fd5c14f7fffe81764a06c3e2c58be9f99afb
ecuellog <ecuellog@gmail.com>
2017-11-24 00:54:47 +0000

    First controller working

--- public/directives/navBar.js
+++ public/directives/navBar.js
@@ -4,7 +4,8 @@ myApp.directive("navBar", function(){
 		templateUrl: "public/directives/navBar.html"
 	};
 });
-myApp.controller("navBarCtrl", [$scope, function($scope){
+
+myApp.controller("navBarCtrl", ["$scope", function($scope){
 	$scope.accountStatus = "Login";
 }]);
 
